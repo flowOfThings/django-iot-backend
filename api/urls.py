b0e5299e06a7a1cbe9f1b3e8bf88411c890356c6
Karri <karrit@proton.me>
2026-01-05 14:10:33 +0200

    Added HttpResponse to api urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
+    path("ping", lambda r: HttpResponse("pong")),
     path('sensor', views.ingest, name='sensor'),  # must match /api/sensor
     path('data/', views.list_data, name='list_data'),
     path('login', views.login),
